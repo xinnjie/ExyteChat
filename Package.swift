9601cd5715e2c3ee97d4a40768fdec2d8b059959
xinnjie <xinnjie31@gmail.com>
2026-04-24 13:11:41 +0800

    add platform macOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ let package = Package(
     name: "Chat",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v17)
+        .iOS(.v17),
+        .macOS(.v14)
     ],
     products: [
         .library(
